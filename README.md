c00567e23a9335a51afc7fb0420a0f84d57e9c11
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-07-19 00:46:14 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# titanic-data-processing
+This is a data processing python script for AGU class of data mining.
---
+++ script.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+TRAIN_DATA_PATH = "train.csv"
+TEST_DATA_PATH = "test.csv"
+
+if __name__ == "__main__":
+    pass
